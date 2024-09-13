3739d93dd3bf8c9b01a1c0e20e70a673640b0584
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-09-13 14:07:47 +0300

    Fix Cal.com Autoscroll

---
+++ src/components/Cal.jsx
@@ -0,0 +1,75 @@
+// import Cal, { getCalApi } from "@calcom/embed-react"
+// import { useEffect } from "react"
+
+// export default function MyApp() {
+//   useEffect(() => {
+//     ;(async function () {
+//       const cal = await getCalApi({ namespace: "on-gorusme" })
+//       cal("ui", {
+//         styles: { branding: { brandColor: "#000000" } },
+//         hideEventTypeDetails: false,
+//         layout: "month_view",
+//       })
+//     })()
+//   }, [])
+//   return (
+//     <Cal
+//       namespace="on-gorusme"
+//       calLink="ruyadigital/on-gorusme"
+//       style={{ width: "100%", height: "100%", overflow: "scroll" }}
+//       config={{ layout: "month_view" }}
+//     />
+//   )
+// }
+
+import { useEffect, useState } from "react"
+import Cal, { getCalApi } from "@calcom/embed-react"
+
+const CalComponent = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true)
+          observer.disconnect() // stop observing once the component is visible
+        }
+      },
+      { threshold: 0.1 } // trigger when 10% of the section is visible
+    )
+
+    const section = document.getElementById("iletisim")
+    if (section) {
+      observer.observe(section)
+    }
+
+    return () => {
+      if (section) observer.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isVisible) {
+      ;(async () => {
+        const cal = await getCalApi({ namespace: "on-gorusme" })
+        cal("ui", {
+          styles: { branding: { brandColor: "#000000" } },
+          hideEventTypeDetails: false,
+          layout: "month_view",
+        })
+      })()
+    }
+  }, [isVisible])
+
+  return isVisible ? (
+    <Cal
+      namespace="on-gorusme"
+      calLink="ruyadigital/on-gorusme"
+      style={{ width: "100%", height: "100%", overflow: "scroll" }}
+      config={{ layout: "month_view" }}
+    />
+  ) : null // Don't render anything if not visible
+}
+
+export default CalComponent
