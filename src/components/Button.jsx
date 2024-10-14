ad85a1d363be29e8102dcdf911816a0a5beb391c
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-10-14 18:43:35 +0300

    feat: Add 404 page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
-export default function Button({ href = "/#iletisim" }) {
+export default function Button({
+  href = "/#iletisim",
+  text = "Ücretsiz Keşif Toplantısı >",
+}) {
   return (
     <a
       className="px-8 py-3 uppercase bg-primary text-white font-bold tracking-wide text-xs rounded-default"
       href={href}
     >
-      Ücretsiz Keşif Toplantısı &gt;
+      {text}
     </a>
   )
 }
